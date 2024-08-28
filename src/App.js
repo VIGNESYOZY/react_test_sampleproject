28168108fc3ecb77fdb5ef775a68de15b309cbb0
VIGNESYOZY <103988535+VIGNESYOZY@users.noreply.github.com>
2024-08-28 17:11:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-    {aws error}
+   
         <h1>this is main branch </h1>
       </header>
       
